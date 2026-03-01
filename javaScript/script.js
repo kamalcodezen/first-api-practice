97da8c6ddf2ef3e6a90ba842ea1441bdbe1fdd54
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-02 00:40:33 +0530

    fetch api to give data

--- javaScript/script.js
+++ javaScript/script.js
@@ -12,13 +12,20 @@ const person = {
 
 // json convert to stringify
 let personJson = JSON.stringify(person);
-console.log(personJson);
+// console.log(personJson);
 
 // json stringify to perse object
 let perseJson = JSON.parse(personJson);
-console.log(perseJson);
+// console.log(perseJson);
 
 
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//     .then(response => response.json())
-//     .then(json => console.log(json))
+
+// api check and give data
+
+
+const submitApi = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+}
+
