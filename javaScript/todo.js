a998828eef0789721c86c63bc6b35e8c42859b84
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-02 14:54:53 +0530

    todo api get and display title and checkbox

--- javaScript/todo.js
+++ javaScript/todo.js
@@ -1,12 +1,37 @@
 
 
 const todosList = () => {
-
     const url = "https://jsonplaceholder.typicode.com/todos";
-
     fetch(url)
         .then((res) => res.json())
-        .then((jsonData) => console.log(jsonData))
+        .then((jsonData) => displayTodo(jsonData));
 
 }
+
+//   {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "delectus aut autem",
+//     "completed": false
+//   },
+
+const displayTodo = (todos) => {
+    // console.log(todos)
+    const todosContainer = document.getElementById("todos-container");
+    todosContainer.innerHTML = "";
+
+    todos.forEach(todo => {
+        // console.log(todo);
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="todo-card">
+        <div>${todo.completed == true ? `<i class="fa-solid fa-square-check"></i>` : `<i class="fa-regular fa-square-check"></i>`}</div>
+        <div>${todo.title}</div>
+        </div>
+        `;
+        todosContainer.append(div);
+    });
+
+}
+
 todosList();
