176319ca69130dcc7acea03a3f023453ce3d5336
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-02 12:25:06 +0530

    fetch api posts complete

--- javaScript/posts.js
+++ javaScript/posts.js
@@ -10,20 +10,30 @@ function postData() {
         })
 }
 
+// {
+//     "userId": 10,
+//     "id": 100,
+//     "title": "at nam consequatur ea labore ea harum",
+//     "body": "cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut"
+// }
+
 const displayData = (posts) => {
 
-    // access container
-    const postContainer = document.getElementById("post-data");
+    // access the container 
+    const postContainer = document.getElementById("post-container");
+    postContainer.innerHTML = "";
 
     posts.forEach(post => {
-        // console.log(post.title);
+        const div = document.createElement("div");
+        div.innerHTML = `
+           <div class="post-card">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>
+        `;
+        postContainer.appendChild(div);
 
-        // create html element
-        const li = document.createElement("li");
-        li.innerText = post.title
-
-        // add li to container
-        postContainer.appendChild(li);
     });
 
-}
+};
+postData();
